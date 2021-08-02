c067586659e88f2b745f5389dd2805d94234281f
Novica Petrovic <novica.petrovic@clearscore.com>
2021-08-02 15:42:46 +0100

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Novica Petrovic on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
