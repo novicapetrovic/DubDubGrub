9c83ba6400a7df79e7ec9887dea59ed1ac19f8d4
Novica Petrovic <novica.petrovic@clearscore.com>
2021-08-03 18:44:15 +0100

    Location Detail View

---
+++ DubDubGrub/LocationDetailView.swift
@@ -0,0 +1,132 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrub
+//
+//  Created by Novica Petrovic on 03/08/2021.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 16) {
+                Image("default-banner-asset")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 120)
+                
+                HStack {
+                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Text("It's Chipotle. Enough said.")
+                    .lineLimit(3)
+                    .minimumScaleFactor(0.75)
+                    .padding(.horizontal)
+                
+                ZStack {
+                    Capsule()
+                        .frame(height: 80)
+                        .foregroundColor(Color(.secondarySystemBackground))
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                        }
+                        
+                        Link(destination: URL(string: "https://www.apple.com")!, label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "network")
+                        })
+                        
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                
+                Text("Who's here?")
+                    .bold()
+                    .font(.title2)
+                
+                LazyVGrid(columns: columns, content: {
+                    FirstNameAvatarView(firstName: "Novica")
+                    FirstNameAvatarView(firstName: "Dan")
+                    FirstNameAvatarView(firstName: "Rob")
+                    FirstNameAvatarView(firstName: "Damien")
+                    FirstNameAvatarView(firstName: "Aidan")
+                })
+                
+                Spacer()
+            }
+            .navigationBarTitle("Chipotle", displayMode: .inline)
+            .toolbar(content: {
+                Button("Dismiss") {
+                    print("Dismiss Tapped")
+                }
+            })
+        }
+        .accentColor(.brandPrimary)
+        .colorScheme(.dark)
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView()
+    }
+}
+
+struct LocationActionButton: View {
+    
+    var color: Color
+    var imageName: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 60, height: 60)
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.white)
+                .frame(width: 22, height: 22)
+        }
+    }
+}
+
+struct FirstNameAvatarView: View {
+    
+    var firstName: String
+    
+    var body: some View {
+        VStack {
+            AvatarView(size: 64)
+            
+            Text(firstName)
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.75)
+        }
+    }
+}
